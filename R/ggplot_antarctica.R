0d5e4849bd80d31df78f6585f39585b121c26cf7
SebastianRothaug <sebastian-rothaug@gmx.de>
2025-03-27 11:31:28 +0100

    # plot function

--- R/ggplot_antarctica.R
+++ R/ggplot_antarctica.R
@@ -2,17 +2,43 @@
 
 
 
+plot_season <- function(season) {
 
-ggplot() +
-  # Plot the minimum ice map in red
-  geom_raster(data = load_sea_ice("summer"), aes(x = x, y = y, fill = ice_thickness)) +  # Use the correct column name
-  scale_fill_gradient(low = "white", high = "red") +  # Color scale for the raster
-  # Plot the maximum ice map in blue
-  geom_raster(data = load_sea_ice("winter"), aes(x = x, y = y, fill = ice_thickness), alpha = 0.5) +  # Use the correct column name
-  scale_fill_gradient(low = "white", high = "blue") +  # Color scale for the raster
-  # Plot Antarctica in white (assuming Antarctica_repo is a vector object, use geom_sf)
-  geom_sf(data = load_antarctica(), fill = "green", color = "green", size = 0.5) +
-  # Customize the plot
-  theme_minimal() +
-  theme(legend.position = "none") +
-  labs(title = "Ice Map Min (Red), Ice Map Max (Blue), Antarctica (White)")
+  if (season == "summer") {
+  season_plot <- ggplot2::ggplot() +
+    # Plot the minimum ice map in red
+    ggplot2::geom_raster(data = load_sea_ice("summer"), ggplot2::aes(x = x, y = y, fill = ice_thickness)) +  # Use the correct column name
+    ggplot2::scale_fill_gradient(low = "white", high = "red") +  # Color scale for the raster
+    # Plot Antarctica in white (assuming Antarctica_repo is a vector object, use geom_sf)
+    ggplot2::geom_sf(data = load_antarctica(), fill = "gray", color = "gray", size = 0.5) +
+    # Customize the plot
+    ggplot2::theme_minimal() +
+    ggplot2::theme(legend.position = "none") +
+    ggplot2::labs(title = "Ice Map Min (Red), Ice Map Max (Blue), Antarctica (White)")
+  }
+
+  if (season == "winter") {
+    season_plot <- ggplot2::ggplot() +
+      # Plot the minimum ice map in red
+      ggplot2::geom_raster(data = load_sea_ice("winter"), ggplot2::aes(x = x, y = y, fill = ice_thickness)) +  # Use the correct column name
+      ggplot2::scale_fill_gradient(low = "white", high = "blue") +  # Color scale for the raster
+      # Plot Antarctica in white (assuming Antarctica_repo is a vector object, use geom_sf)
+      ggplot2::geom_sf(data = load_antarctica(), fill = "gray", color = "gray", size = 0.5) +
+      # Customize the plot
+      ggplot2::theme_minimal() +
+      ggplot2::theme(legend.position = "none") +
+      ggplot2::labs(title = "Ice Map Min (Red), Ice Map Max (Blue), Antarctica (White)")
+  }
+
+  else {
+    stop("Invalid season. Choose either 'summer' or 'winter'.")
+  }
+
+  return(season_plot)
+}
+
+
+####
+antseason <- plot_season("winter")
+
+antseason
