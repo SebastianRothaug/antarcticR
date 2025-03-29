e4cbc19ab14fc0e64cb77d2bb3418416a9c3abab
SebastianRothaug <sebastian-rothaug@gmx.de>
2025-03-29 19:52:26 +0100

    # roxy update

--- R/load_sea_ice.R
+++ R/load_sea_ice.R
@@ -1,27 +1,38 @@
-#' load_country_boundary
+#' Load Sea Ice Data for a Specific Season
 #'
-#' #Load the boundary of a specific country from a map object
-#' #This function extracts the boundary geometry of a specific country from a map
-#' #object and returns it
+#' This function loads and processes sea ice data for either the summer or winter season,
+#' crops the data to a specific geographic extent, projects it to the Antarctic Polar Stereographic
+#' coordinate reference system (EPSG:3031), and returns it as a data frame suitable for visualization.
 #'
+#' @param season A string indicating the season. Accepts `"summer"` or `"winter"`.
 #'
-#' @param map A spatial object containing country boundaries, such as a 'sf' object
-#' @param country_name The name of the country whose boundary is to be loaded
+#' @return A data frame containing the ice thickness data for the specified season. The data frame
+#'   will have columns `x`, `y`, and `ice_thickness` representing the spatial coordinates and ice thickness,
+#'   respectively.
 #'
-#' @return
-#' @export
-#'
-#' @examples
-#' library(sf)
-#' map <- ne_download(scale = 10, returnclass = "sf")
-#' country_boundary <- load_country_boundary(map, "Denmark")
-#' plot(country_boundary)
+#' @details
+#' - For **summer**, it loads the minimum ice thickness data (`BO21_icethickltmin_ss`).
+#' - For **winter**, it loads the maximum ice thickness data (`BO21_icethickltmax_ss`).
+#' - The data is cropped to the region between longitudes -180 to 180 and latitudes -90 to -45.
+#' - The resulting data is projected to EPSG:3031 (Antarctic Polar Stereographic).
 #'
 #' @importFrom sdmpredictors load_layers
+#' @importFrom terra crop
 #' @importFrom raster projectRaster
+#' @importFrom raster as.data.frame
 #'
+#' @examples
+#' \dontrun{
+#' load_sea_ice("summer")
+#' load_sea_ice("winter")
+#' }
+#'
+#' @seealso \code{\link{plot_season}}
+#'
+#' @author Your Name
 #' @export
-#'
+
+
 load_sea_ice <- function(season) {
 
   if (season == "summer") {
@@ -45,8 +56,6 @@ load_sea_ice <- function(season) {
 }
 
 
-###
-summer_ice <- load_sea_ice("summer")
+seaIceSummer <- load_sea_ice("summer")
 
-
-View(summer_ice)
+plot(seaIceSummer)
