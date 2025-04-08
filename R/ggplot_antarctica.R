b15fd0128ffedf6abd13160aedad9d6a9851b468
SebastianRothaug <sebastian-rothaug@gmx.de>
2025-04-08 13:29:50 +0200

    # antarcticR::

--- R/ggplot_antarctica.R
+++ R/ggplot_antarctica.R
@@ -21,7 +21,7 @@
 #'
 #' @seealso \code{\link{load_sea_ice}}, \code{\link{load_continent}}
 #'
-#' @author Your Name
+#' @author Sebastian Rothaug
 #' @export
 
 
@@ -31,10 +31,10 @@ plot_season <- function(season) {
   if (season == "summer") {
   season_plot <- ggplot2::ggplot() +
     # Plot the minimum ice map in red
-    ggplot2::geom_raster(data = load_sea_ice("summer"), ggplot2::aes(x = x, y = y, fill = ice_thickness)) +  # Use the correct column name
+    ggplot2::geom_raster(data = antarcticR::load_sea_ice("summer"), ggplot2::aes(x = x, y = y, fill = ice_thickness)) +  # Use the correct column name
     ggplot2::scale_fill_gradient(low = "white", high = "blue") +  # Color scale for the raster
     # Plot Antarctica in white (assuming Antarctica_repo is a vector object, use geom_sf)
-    ggplot2::geom_sf(data = load_continent(), fill = "gray", color = "gray", size = 0.5) +
+    ggplot2::geom_sf(data = antarcticR::load_continent(), fill = "gray", color = "gray", size = 0.5) +
     # Customize the plot
     ggplot2::theme_minimal() +
     ggplot2::theme(legend.position = "none") +
@@ -46,10 +46,10 @@ plot_season <- function(season) {
   else if (season == "winter") {
     season_plot <- ggplot2::ggplot() +
       # Plot the minimum ice map in red
-      ggplot2::geom_raster(data = load_sea_ice("winter"), ggplot2::aes(x = x, y = y, fill = ice_thickness)) +  # Use the correct column name
+      ggplot2::geom_raster(data = antarcticR::load_sea_ice("winter"), ggplot2::aes(x = x, y = y, fill = ice_thickness)) +  # Use the correct column name
       ggplot2::scale_fill_gradient(low = "white", high = "blue") +  # Color scale for the raster
       # Plot Antarctica in white (assuming Antarctica_repo is a vector object, use geom_sf)
-      ggplot2::geom_sf(data = load_continent(), fill = "gray", color = "gray", size = 0.5) +
+      ggplot2::geom_sf(data = antarcticR::load_continent(), fill = "gray", color = "gray", size = 0.5) +
       # Customize the plot
       ggplot2::theme_minimal() +
       ggplot2::theme(legend.position = "none") +
@@ -62,3 +62,4 @@ plot_season <- function(season) {
 
   return(season_plot)
 }
+
