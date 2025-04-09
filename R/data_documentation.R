51792a65b5dfb1b5839bd83a72e889f519ec00ff
SebastianRothaug <sebastian-rothaug@gmx.de>
2025-04-09 15:40:19 +0200

    # push everything

---
+++ R/data_documentation.R
@@ -0,0 +1,13 @@
+#' Antarctic Coastline Data
+#'
+#' This dataset contains the Antarctic coastline as a spatial object.
+#'
+#' @format An `sf` object with X rows and Y columns (adjust to your dataset)
+#' @usage data(antarctic_coastline)
+#' @keywords datasets
+#' @examples
+#' \dontrun{
+#' data(antarctic_coastline)
+#' plot(antarctic_coastline)
+#' }
+"antarctic_coastline"
