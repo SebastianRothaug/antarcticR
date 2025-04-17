2821ea5027fc661d35280f0d7d14047d1d6aab52
SebastianRothaug <sebastian-rothaug@gmx.de>
2025-04-17 10:42:58 +0200

    # add ice shelves

---
+++ R/data_doc_ice_shelves.R
@@ -0,0 +1,13 @@
+#' Antarctic Ice Shelves Data
+#'
+#' This dataset contains the Antarctic Ice Shelves as a spatial object.
+#'
+#' @format An `sf` object with X rows and Y columns (adjust to your dataset)
+#' @usage data(antarctic_ice_shelves)
+#' @keywords datasets
+#' @examples
+#' \dontrun{
+#' data(antarctic_ice_shelves)
+#' plot(antarctic_ice_shelves)
+#' }
+"antarctic_ice_shelves"
---
+++ R/load_ice_shelves.R
@@ -0,0 +1,23 @@
+#' Load Antarctic Ice Shelf Data
+#'
+#' This function loads the Antarctic Ice Shelf dataset.
+#' The data derived from naturalearthdata Download Service.
+#'
+#' @return A spatial or simple features (`sf`) object containing all the Antarctic Ice Shelves.
+#'
+#' @examples
+#' \dontrun{
+#' ice_shelves <- load_ice_shelves()
+#' plot(ice_shelves)
+#' }
+#'
+#' @export
+load_ice_shelves <- function() {
+  data_env <- new.env()
+
+  # Load dataset into the environment
+  data("antarctic_ice_shelves", package = "antarcticR", envir = data_env)
+
+  # Retrieve and return the dataset
+  return(get("antarctic_ice_shelves", envir = data_env))
+}
